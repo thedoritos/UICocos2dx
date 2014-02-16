e08536efd8a14ba1048aa41b0c63fd886d6b4ff9
thedoritos <thedoritos@gmail.com>
2014-02-16 16:13:50 +0900

    Replacing AppDelegate
    So it launches like an ordinal iOS App.

---
+++ proj.ios_mac/Classes.ios/Controllers/AppDelegate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UICocos2dx
+//
+//  Created by thedoritos on 2014/02/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
